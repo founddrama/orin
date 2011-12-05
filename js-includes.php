8ad9bacbcda14f63449a33063b85348675ca7643
Rob Friesel <rob@founddrama.net>
2011-12-04 22:20:37 -0500

    bump jQuery loader to point at 1.7.1

--- js-includes.php
+++ js-includes.php
@@ -8,5 +8,5 @@
  */
 
 ?>
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 <?php /* <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/orin.min.js"></script> */ ?>
