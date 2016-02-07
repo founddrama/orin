537106a33b835e0d5da1e6212d99673fb1f0c37c
Rob Friesel <rob@founddrama.net>
2016-02-06 20:05:42 -0500

    (remember! fix srcset)

--- functions.php
+++ functions.php
@@ -346,3 +346,17 @@ function orin_edit_post_link() {
 	edit_post_link( __( '&#9998; Edit', 'orin' ), '', '' );
 }
 endif;
+
+
+/**
+ * Force URLs in srcset attributes into HTTPS scheme.
+ * source: https://wordpress.org/support/topic/responsive-images-src-url-is-https-srcset-url-is-http-no-images-loaded?replies=19#post-7767555
+ */
+function ssl_srcset( $sources ) {
+  foreach ( $sources as &$source ) {
+    $source['url'] = set_url_scheme( $source['url'], 'https' );
+  }
+
+  return $sources;
+}
+add_filter( 'wp_calculate_image_srcset', 'ssl_srcset' );
