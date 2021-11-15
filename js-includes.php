6b5f0d2f249253cc955a28b36a43131110d58649
Rob Friesel <rob@founddrama.net>
2021-11-14 23:18:19 -0500

    feat: removes jQuery   I don't think Orin was *ever* actually dependent on it

--- js-includes.php
+++ js-includes.php
@@ -8,5 +8,4 @@
  */
 
 ?>
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/iframe-resizer.js"></script>
